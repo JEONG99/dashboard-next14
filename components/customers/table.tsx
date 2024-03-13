7c2053cbe51bd714c4668b77c675768799a240c2
jeong99 <jbh990621@gmail.com>
2024-03-13 13:37:59 +0900

    fix: lusitana path ui/fonts -> styles/fonts

--- components/customers/table.tsx
+++ components/customers/table.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '@/styles/fonts';
 import Search from '@/components/search';
 import {
   CustomersTableType,
