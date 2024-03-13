47aded09a199ff17a95065c92d2cf5e7b113a088
jeong99 <jbh990621@gmail.com>
2024-03-13 16:51:18 +0900

    ch16: Adding metadata

---
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -0,0 +1,39 @@
+import Form from '@/components/invoices/edit-form';
+import Breadcrumbs from '@/components/invoices/breadcrumbs';
+import { fetchCustomers, fetchInvoiceById } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
+import { Metadata } from 'next';
+
+export const metadata: Metadata = {
+  title: 'Edit Invoice',
+};
+
+export default async function EditInvoicePage({
+  params: { id },
+}: {
+  params: { id: string };
+}) {
+  const [invoice, customers] = await Promise.all([
+    fetchInvoiceById(id),
+    fetchCustomers(),
+  ]);
+
+  if (!invoice) {
+    notFound();
+  }
+  return (
+    <main>
+      <Breadcrumbs
+        breadcrumbs={[
+          { label: 'Invoices', href: '/dashboard/invoices' },
+          {
+            label: 'Edit Invoice',
+            href: `/dashboard/invoices/${id}/edit`,
+            active: true,
+          },
+        ]}
+      />
+      <Form invoice={invoice} customers={customers} />
+    </main>
+  );
+}
